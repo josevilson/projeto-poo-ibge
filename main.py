35ab98617b7d27d59abb6f538461bdbd3f9b357c
Jose Vilson <contatojosevilson@gmail.com>
2024-09-02 21:39:38 -0300

    transform to dataframe

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # %%
 from src import DataTransform, DataExtractor, DataLoader
+# from sqlite3 import SQLite
 
 indicadores = "77836|77819"
 paises = "AR|BR"
 
 extractor = DataExtractor(indicadores=indicadores, paises=paises)
 dados = extractor.get_data()
+
+
+transform = DataTransform()
+json = transform.data_to_json(dados)
+
+
+df = transform.transform_to_dataframe()
+print(df)
 # %%
--- src/transform.py
+++ src/transform.py
@@ -1,3 +1,28 @@
+import pandas as pd
+import json
+
 class DataTransform:
-    def __init__(self, data):
-        pass
+    def __init__(self):
+        self.data = None
+    
+    def data_to_json(self, json_data):
+        records = []
+        for item in json_data:
+            indicador = item['indicador']
+            for serie in item['series']:
+                pais = serie['pais']['nome']
+                for entry in serie['serie']:
+                    for ano, valor in entry.items():
+                        if valor is not None:
+                            records.append({
+                                'Indicador': indicador,
+                                'País': pais,
+                                'Ano': ano,
+                                'Valor': valor,
+                                })
+            self.data = records
+        return records
+
+    def transform_to_dataframe(self):
+        df = pd.json_normalize(self.data)
+        return df
