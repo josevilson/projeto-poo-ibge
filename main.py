21ed71c1a0c07265f22a35d675c68a847f0d81f0
Jose Vilson <contatojosevilson@gmail.com>
2024-09-06 18:44:42 -0300

    secrets.toml

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ paises_selecionados = st.multiselect(label="Selecione os paises de interesse.",
                                      default=['BR - Brasil'])
 
 
-username_postgres = os.environ['POSTGRES_USERNAME']
-password_postgres = os.environ['POSTGRES_PASSWORD']
-host_postgres = os.environ['POSTGRES_HOST']
-port_postgres = os.environ['POSTGRES_PORT']
-dbname_postgres = os.environ['POSTGRES_DB']
+username_postgres = st.secrets['POSTGRES_USERNAME']
+password_postgres = st.secrets['POSTGRES_PASSWORD']
+host_postgres = st.secrets['POSTGRES_HOST']
+port_postgres = st.secrets['POSTGRES_PORT']
+dbname_postgres = st.secrets['POSTGRES_DB']
 
 string_connection_postgres = f'postgresql+psycopg2://{username_postgres}:{
     password_postgres}@{host_postgres}:{port_postgres}/{dbname_postgres}'
